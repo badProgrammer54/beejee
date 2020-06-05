5e47113e62e4b91cf038de3010a358168b7a756f
rubanov13 <ruban1988@mail.ru>
2020-06-05 17:55:38 +0700

    изменил db

--- db.sql
+++ db.sql
@@ -2,14 +2,10 @@ CREATE TABLE `tasks` (
   `id` int(11) NOT NULL,
   `name` varchar(30) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `text` text NOT NULL
+  `text` text NOT NULL,
+  `done` tinyint(4) NOT NULL,
+  `is_edit` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `users` (
-  `login` varchar(30) NOT NULL,
-  `hash` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 
 ALTER TABLE `tasks`
   ADD PRIMARY KEY (`id`);
